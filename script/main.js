70de670808a132330a03a184ffc8f181fbdc8995
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-05-23 11:02:52 +0200

    done

---
+++ script/main.js
@@ -0,0 +1,22 @@
+// - Create a function called "greet" that accepts a single parameter. Log the parameter along with a "Hello".
+function greet(params) {
+    console.log("Hello " + params);
+}
+greet("Winni the Pooh Bear")
+
+// - Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
+function multiply(num1,num2) {
+    console.log(num1 * num2);
+}
+multiply(2,4)
+
+// - Create a function called tableOfFive which accepts a single parameter. 
+//   Return the parameter multiplied by five to a variable, and log this variable.
+function tableOfFive(num) {
+    return num * 5;
+}
+
+let result = tableOfFive(2)
+
+console.log(result);
+
